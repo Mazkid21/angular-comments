111d4fbe5f359bfca139f4773942aa2ef93daf98
Mazkid21 <zmaraziti@yahoo.com>
2017-08-17 00:34:32 -0600

    got text feilds but not updating on save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,62 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+
+class quote{
+  update: false;
+	text: string;
+	author:string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
+  updateText; 
+  updateAuthor;
+  newQuote:quote;
+	editToggle = false;
+	constructor(){
+		this.newQuote = new quote();
+	}
+  comments  =[
+      {update:false, text: 'first comment!', author: 'joe'},
+      {update:false, text: 'nice work!', author: 'tim'},
+      {update:false, text: 'I would also like to congratulate you!', author: 'steve'}
   ];
+
+
+
+onSubmit(){
+	if(this.newQuote){
+		var entry = {
+			'update': true,
+      'text': this.newQuote.text,
+			'author': this.newQuote.author
+		};
+		this.comments.push(entry)
+	}
+	
+}
+
+  onDelete(i) {
+    if (confirm('Are you sure?')) {
+      this.comments.splice(i, 1);
+    }
+  }
+
+  onsubmit(i){
+  	 if(this.updateText && this.updateAuthor){
+    var entry = {
+      'update': true,
+      'text': this.updateText,
+      'author': this.updateAuthor
+    };
+    this.comments.push(entry)
+  }
+}
 }
